4634283a1701c8916fdb62dcda186c6c6dc4ed01
Elvis Pham <epha1206@gmail.com>
2018-05-08 20:15:08 -0400

    Added data to friends

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+//data structure stores the friends data
+var friendsArray = [
+   {
+       name: 'Michael Scott',
+       photo: 'https://img.buzzfeed.com/buzzfeed-static/static/2016-07/22/18/campaign_images/buzzfeed-prod-fastlane01/michael-scott-quotes-that-are-still-hilarious-2-10277-1469225647-3_dblbig.jpg',
+       scores: [
+           '1',
+           '5',
+           '5',
+           '1',
+           '5',
+           '5',
+           '1',
+           '1',
+           '1',
+           '5'
+       ]
+   },
+   {
+       name: 'Dwight Shrute',
+       photo: 'https://i.pinimg.com/originals/87/63/28/87632831c5d19307968e1a3a6a9df49d.jpg',
+       scores: [
+           '2',
+           '4',
+           '1',
+           '1',
+           '1',
+           '1',
+           '1',
+           '2',
+           '1',
+           '5'
+       ]
+   },
+   {
+       name: 'Erin Hannon',
+       photo: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Erin_Hannon.jpg/250px-Erin_Hannon.jpg',
+       scores: [
+           '4',
+           '3',
+           '5',
+           '1',
+           '1',
+           '5',
+           '5',
+           '1',
+           '1',
+           '1'
+       ]
+   },
+   {
+       name: 'Pam Beesly',
+       photo: 'http://images2.fanpop.com/image/photos/11400000/Pam-pam-halpert-11469678-330-495.jpg',
+       scores: [
+        '5',
+        '1',
+        '2',
+        '1',
+        '3',
+        '4',
+        '1',
+        '3',
+        '3',
+        '4'
+       ]
+   },
+   {
+       name: 'Jim Halpert',
+       photo: 'https://pbs.twimg.com/profile_images/37658942/Jim_400x400.jpg',
+       scores: [
+        '1',
+        '1',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '1' 
+       ]
+   },
+   {
+       name: 'Kevin Malone',
+       photo: 'https://pbs.twimg.com/profile_images/762958606199320576/WEa6ttXr_400x400.jpg',
+       scores: [
+        '1',
+        '1',
+        '5',
+        '1',
+        '3',
+        '2',
+        '5',
+        '1',
+        '1',
+        '5'
+       ]
+   },
+   {
+       name: 'Angela Martin',
+       photo: 'https://i.pinimg.com/originals/76/8b/f4/768bf4445341338d12a0787ca0991913.jpg',
+       scores: [
+        '5',
+        '4',
+        '4',
+        '2',
+        '4',
+        '4',
+        '3',
+        '2',
+        '3',
+        '3'
+       ]
+   },
+   {
+       name: 'Kelly Kapoor',
+       photo: 'https://media1.popsugar-assets.com/files/thumbor/eXDFklp6xdWXLyvn9BcH5p-b_rk/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2016/06/21/951/n/1922283/05e3e789_edit_img_image_41730517_1466542783_kelly-th/i/Kelly-Kapoor-GIFs-From-Office.jpg',
+       scores: [
+        '5',
+        '2',
+        '3',
+        '1',
+        '5',
+        '1',
+        '3',
+        '1',
+        '1',
+        '5'
+       ]
+   },
+   {
+       name: 'Ryan Howard',
+       photo: 'https://i.pinimg.com/originals/07/e0/4e/07e04e18071f5c991852bbd26e7b3739.jpg',
+       scores: [
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5',
+        '5'
+       ]
+   },
+   {
+       name: 'Meredith Palmer',
+       photo: 'https://pbs.twimg.com/profile_images/597610431293816832/TjUXTmTj_400x400.jpg',
+       scores: [
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1',
+        '1'
+       ]
+   }
+];
+
+module.exports = friendsArray;
